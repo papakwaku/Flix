f31fe20dd69a9bf1256a8851a626323df8b5228f
Papa <pawuahmantey@gmail.com>
2020-01-29 19:46:00 -0500

    Initial Commit

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Papa Kwaku  on 1/28/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
